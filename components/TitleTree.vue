b515e760b4e0f8dafa9ca73f6198c071c10cdf49
nakahararuu <repika02@gmail.com>
2018-11-10 12:18:07 +0900

    Style: improve spacing

--- components/TitleTree.vue
+++ components/TitleTree.vue
@@ -2,14 +2,16 @@
     <v-expansion-panel>
         <v-expansion-panel-content v-for="(title, i) in titles" :key="i">
             <div slot="header" @click="fetchSubtitles(title)">{{title.title}}</div>
-            <ul>
-                <li class="subtitle"
-                    v-for="subTitle in title.subTitles"
-                    @click="$emit('selected-subtitle', {title: title, subTitle:subTitle})"
-                >
-                    {{subTitle.title}}
-                </li>
-            </ul>
+            <v-flex py-3 pl-5>
+                <ul>
+                    <li class="subtitle"
+                        v-for="subTitle in title.subTitles"
+                        @click="$emit('selected-subtitle', {title: title, subTitle:subTitle})"
+                    >
+                        {{subTitle.title}}
+                    </li>
+                </ul>
+            </v-flex>
         </v-expansion-panel-content>
     </v-expansion-panel>
 </template>
@@ -17,7 +19,7 @@
 <style>
     .subtitle {
         cursor: pointer;
-        margin-left: 50px;
+        margin: 8px 0;
     }
 </style>
 
