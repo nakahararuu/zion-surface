17ca71bfc304dc4d8d68f20017c0d9561107918f
nakahararuu <repika02@gmail.com>
2018-11-10 12:18:29 +0900

    Style: improve video style

--- pages/video.vue
+++ pages/video.vue
@@ -1,54 +1,24 @@
 <template>
-    <v-layout column justify-center align-center>
-        <v-flex ms12>
-            <div class="video-player-box"
-                 :playsinline="playsinline"
-                 @play="onPlayerPlay($event)"
-                 @pause="onPlayerPause($event)"
-                 @ended="onPlayerEnded($event)"
-                 @loadeddata="onPlayerLoadeddata($event)"
-                 @waiting="onPlayerWaiting($event)"
-                 @playing="onPlayerPlaying($event)"
-                 @timeupdate="onPlayerTimeupdate($event)"
-                 @canplay="onPlayerCanplay($event)"
-                 @canplaythrough="onPlayerCanplaythrough($event)"
-                 @ready="playerReadied"
-                 @statechanged="playerStateChanged($event)"
-                 v-video-player:myVideoPlayer="playerOptions">
-            </div>
-        </v-flex>
-    </v-layout>
+        <div class="video-player-box"
+             @ended="onPlayerEnded($event)"
+             v-video-player:myVideoPlayer="playerOptions">
+        </div>
 </template>
-
-<style>
-    .video-js {
-        width: 100vw;
-        max-width: 100%;
-        max-height: 100%;
-    }
-
-</style>
 
 <script>
   export default {
-    data () {
-      return {
-        // component options
-        playsinline: true
-      }
-    },
     computed: {
       playerOptions: function () {
-        // videojs options
         return {
           muted: true,
-          language: 'en',
+          preload: 'auto',
+          language: 'ja',
+          fluid: true,
           playbackRates: [0.7, 1.0, 1.5, 2.0],
           sources: [{
             type: 'video/mp4',
             src: `/movie/${this.title}/${this.subTitle}`
           }]
-          // poster: "/static/images/author.jpg",
         }
       }
     },
@@ -62,56 +32,9 @@
       console.log('this is current player instance object', this.myVideoPlayer)
     },
     methods: {
-      // listen event
-      onPlayerPlay (player) {
-        // console.log('player play!', player)
-      },
-      onPlayerPause (player) {
-        // console.log('player pause!', player)
-      },
       onPlayerEnded (player) {
-        // console.log('player ended!', player)
-      },
-      onPlayerLoadeddata (player) {
-        // console.log('player Loadeddata!', player)
-      },
-      onPlayerWaiting (player) {
-        // console.log('player Waiting!', player)
-      },
-      onPlayerPlaying (player) {
-        // console.log('player Playing!', player)
-      },
-      onPlayerTimeupdate (player) {
-        // console.log('player Timeupdate!', player.currentTime())
-      },
-      onPlayerCanplay (player) {
-        // console.log('player Canplay!', player)
-      },
-      onPlayerCanplaythrough (player) {
-        // console.log('player Canplaythrough!', player)
-      },
-
-      // or listen state event
-      playerStateChanged (playerCurrentState) {
-        console.log('player current update state', playerCurrentState)
-      },
-
-      // player is ready
-      playerReadied (player) {
-        console.log('example 01: the player is readied', player)
+        // TODO 次の動画に
       }
     }
   }
 </script>
-
-<style>
-    .container {
-        width: 60%;
-        margin: 0 auto;
-        padding: 50px 0;
-    }
-
-    .video-player-box {
-        min-height: 200px;
-    }
-</style>
