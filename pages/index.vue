5dbf6e2f8281889cdd274669c5420e07975c2f00
nakahararuu <repika02@gmail.com>
2018-12-24 20:27:53 +0900

    Feature: title search field

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-layout column justify-center>
-    <v-flex xs12 sm8 md6 py-3 pl-5>
-      <recycle-scroller
-        class="scroller"
-        :items="titles"
-        :item-height="50"
-        :prerender="50"
-        key-field="title"
-        page-mode
-      >
-        <div class="title" slot-scope="{ item }">
-          <nuxt-link :to="{ path: `titles/${item.title}`, query: { st: 0 } }">
-            {{ item.title }}
-          </nuxt-link>
-        </div>
-      </recycle-scroller>
-    </v-flex>
-  </v-layout>
+  <div>
+    <v-autocomplete
+      :items="titles.map(t => t.title)"
+      @input="navigateToTitle($event)"
+    />
+    <v-layout column justify-center>
+      <v-flex xs12 sm8 md6 py-3 pl-5>
+        <recycle-scroller
+          class="scroller"
+          :items="titles"
+          :item-height="50"
+          :prerender="50"
+          key-field="title"
+          page-mode
+        >
+          <div class="title" slot-scope="{ item }">
+            <nuxt-link :to="{ path: `titles/${item.title}`, query: { st: 0 } }">
+              {{ item.title }}
+            </nuxt-link>
+          </div>
+        </recycle-scroller>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -24,6 +30,11 @@ export default {
   async asyncData({ app }) {
     const data = await app.$axios.$get('/json/getTitleArray.php')
     return { titles: data.titleArray }
+  },
+  methods: {
+    navigateToTitle: function(event) {
+      this.$router.push({ path: `titles/${event}` })
+    }
   }
 }
 </script>
